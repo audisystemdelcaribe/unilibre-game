d3ae340511d5eb0822b971f1aa80cbfbe99d042d
Freddy Mejía Torres <info@audisystemdelcaribe.com>
2026-03-02 09:21:23 -0500

    ajustes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,17 @@
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
+import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
-    // Fuerza a Astro a actuar como un servidor real
     output: 'server',
     adapter: node({
         mode: 'standalone',
     }),
-    integrations: [svelte()],
+    integrations: [
+        icon()
+    ],
     vite: {
         plugins: [tailwindcss()],
-        server: {
-            fs: {
-                allow: ['.']
-            },
-            host: 'localhost'
-        },
-        optimizeDeps: {
-            exclude: ['@supabase/ssr']
-        }
     },
 });
