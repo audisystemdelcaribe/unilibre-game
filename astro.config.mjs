4efb732960f972249fc8e966cf0866ecb6e7f327
Freddy Mejia <freddymejiatorres@gmail.com>
2026-03-09 17:29:14 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
-import icon from 'astro-icon';
-
 import vercel from '@astrojs/vercel';
+import icon from 'astro-icon';
+// import node from '@astrojs/node';
 
 export default defineConfig({
     output: 'server',
