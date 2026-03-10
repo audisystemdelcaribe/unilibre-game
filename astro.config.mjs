1723e06b21915958d2d72896e9563d7578135965
Freddy Mejia <freddymejiatorres@gmail.com>
2026-03-10 09:45:21 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,10 @@ import vercel from '@astrojs/vercel'; // Cambiamos 'node' por 'vercel'
 import icon from 'astro-icon';
 
 export default defineConfig({
-  output: 'server', // Mantenemos SSR
+    site: 'https://unilibre-game.vercel.app',
+  output: 'server',
   adapter: vercel({
-    // Vercel no necesita el "mode: standalone", 
-    // el adaptador se encarga de todo automáticamente.
-    webAnalytics: { enabled: true } // Opcional: activa analíticas de Vercel
+    webAnalytics: { enabled: true } 
   }),
   security: {
     checkOrigin: false,
